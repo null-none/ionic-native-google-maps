d7ce31b18e3eb262c59b971a5fe9135443c6f0bb
Dmitry Kalinin <dmitry.kalinin.email@gmail.com>
2016-10-26 18:54:10 +0300

    version 0.0.2

--- ionicNativeGoogleMaps.js
+++ ionicNativeGoogleMaps.js
@@ -7,13 +7,61 @@ angular.module('ionicNativeGoogleMaps', [])
       mapReady: '&onMapReady'
     },
     link: function(scope, element, attrs) {
+      var map = null;
+      scope.$root.$watchGroup(['lat', 'lng'], function(newValues, oldValues, scope) {
+        if (map) {
+          map.animateCamera({
+            target: {
+              lat: scope.$root.lat,
+              lng: scope.$root.lng
+            },
+            zoom: 17,
+            tilt: 60,
+            bearing: 140,
+            duration: 5000
+          }, function() {});
+        }
+      });
+
+      function marker(map, item) {
+        map.addMarker({
+          position: {
+            lat: item.latitude,
+            lng: item.longitude
+          },
+          icon: {
+            'url': item.icon
+          },
+          title: item.title,
+          snippet: item.latitude + ' , ' + item.longitude,
+          animation: plugin.google.maps.Animation.BOUNCE
+        }, function(marker) {
+          marker.showInfoWindow();
+        });
+      }
+
       function triggerMapReady(map) {
         scope.mapReady({
           map: map
         });
+
+        map.animateCamera({
+          target: {
+            lat: scope.$root.map.center.latitude,
+            lng: scope.$root.map.center.longitude
+          },
+          zoom: scope.$root.map.zoom,
+          tilt: 60,
+          bearing: 140,
+          duration: 5000
+        }, function() {});
+
         if (attrs.hasOwnProperty('height')) {
           element.css('height', attrs.height)
         }
+        scope.$root.markers.forEach(function(item, i, arr) {
+          marker(map, item);
+        });
       }
       document.addEventListener("deviceready", function() {
         var mapParams = {
@@ -31,7 +79,7 @@ angular.module('ionicNativeGoogleMaps', [])
           }
         };
 
-        var map = plugin.google.maps.Map.getMap(element[0], mapParams);
+        map = plugin.google.maps.Map.getMap(element[0], mapParams);
         map.on(plugin.google.maps.event.MAP_READY, triggerMapReady);
       }, false);
     }
